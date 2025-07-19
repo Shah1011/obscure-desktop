dc324ccb3b4edbee2f5662b12cf9c8b73163207f
Shah1011 <officiallyshah@outlook.com>
2025-07-19 15:14:21 +0530

    feat: DX Improvements

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,15 +1,14 @@
 import {app, BrowserWindow} from 'electron';
+import { isDev } from './util.js';
 import path from 'path';
 
 type test = string;
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    const mainWindow = new BrowserWindow({});
+    if (isDev()){
+        mainWindow.loadURL('http://localhost:5123');
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    }
 });
